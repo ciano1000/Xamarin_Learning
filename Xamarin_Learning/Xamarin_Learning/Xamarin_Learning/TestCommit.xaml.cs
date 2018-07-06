b047d2bc9a5edca307039c6efac11bad0b9e37c4
ciano1000 <ciano1000@gmail.com>
2018-07-06 15:08:35 +0100

    Test commit

---
+++ Xamarin_Learning/Xamarin_Learning/Xamarin_Learning/TestCommit.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_Learning
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TestCommit : ContentPage
+	{
+		public TestCommit ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
