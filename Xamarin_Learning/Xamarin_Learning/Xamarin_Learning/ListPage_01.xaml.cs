77149c148ac52024a4cba2b0bf8eeebee884389f
ciano1000 <ciano1000@gmail.com>
2018-07-24 23:25:37 +0100

    Did more work with Lists

--- Xamarin_Learning/Xamarin_Learning/Xamarin_Learning/ListPage_01.xaml.cs
+++ Xamarin_Learning/Xamarin_Learning/Xamarin_Learning/ListPage_01.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,12 @@ namespace Xamarin_Learning
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ListPage_01 : ContentPage
 	{
+        private ObservableCollection<ContactGroup> _contacts;
 		public ListPage_01 ()
 		{
 			InitializeComponent ();
 
-            listView.ItemsSource = new List<ContactGroup>
+            _contacts = new ObservableCollection<ContactGroup>
             {
                 new ContactGroup("C", "C")
                 {
@@ -30,6 +32,39 @@ namespace Xamarin_Learning
                 
 
             };
+            listView.ItemsSource = _contacts;
 		}
-	}
+
+        private void listView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var contact = e.Item as Contact;
+            DisplayAlert("Tapped", contact.Name, "OK");
+        }
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e) //to disable selection set listView.SelectedItem = null;
+        {
+            var contact = e.SelectedItem as Contact;
+            DisplayAlert("Selected",contact.Name,"OK");
+        }
+
+        private void Call_Clicked(object sender, EventArgs e)
+        {
+            var menu = sender as MenuItem;
+            var contact = menu.CommandParameter as Contact;
+            DisplayAlert("Call", contact.Name, "ok");
+        }
+
+        private void Delete_Clicked(object sender, EventArgs e)
+        {
+            var contact = (sender as MenuItem).CommandParameter as Contact;
+            char firstLetter = contact.Name[0];
+            char.ToUpper(firstLetter);
+           
+            var contactGroup = _contacts.Single(i => i.Title == firstLetter.ToString());
+
+
+            contactGroup.Remove(contact);
+            DisplayAlert("Deleted",contactGroup.Title , "ok");
+        }
+    }
 }
--- Xamarin_Learning/Xamarin_Learning/Xamarin_Learning/Models/ContactGroup.cs
+++ Xamarin_Learning/Xamarin_Learning/Xamarin_Learning/Models/ContactGroup.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace Xamarin_Learning.Models
 {
-    class ContactGroup:List<Contact>
+    class ContactGroup:ObservableCollection<Contact>
     {
         public string Title { get; set; }
         public string ShortTitle { get; set; }
